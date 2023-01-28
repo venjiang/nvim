e05e7f94b813f62a99dbd674efafc5c9f9b5464c
venjiang <venjiang@gmail.com>
2023-01-29 01:44:40 +0800

    update config

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,16 +1,22 @@
---pcall(
---  vim.cmd,
---  [[
---    augroup packer_user_config
---    autocmd!
---    autocmd BufWritePost plugins.lua source <afile> | PackerSync
---    augroup end
---  ]]
---)
-local packer=require('packer')
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    vim.notify('packer installing...')
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+local packer = require('packer')
 
 packer.startup({
   function(use)
+    -- packer
+    use('wbthomason/packer.nvim')
     -- colorscheme
     -- tokyonight
     use('folke/tokyonight.nvim')
@@ -25,12 +31,16 @@ packer.startup({
     -- lualine
     use({'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true}})
     -- treesitter
-    use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
+    --use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
     -- lsp mason
     use({'williamboman/mason.nvim'})
     -- lspconfig
     use({ 'neovim/nvim-lspconfig'})
 
-
+    -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+    if packer_bootstrap then
+      packer.sync()
+    end
   end
 })
